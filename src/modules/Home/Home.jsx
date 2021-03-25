d7ce1ce6fe27f14513ab65aad152eedc11a6a955
ReadyPlayerEmma <52614873+ReadyPlayerEmma@users.noreply.github.com>
2021-03-25 06:47:32 -0500

    Update Home.jsx

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -90,7 +90,9 @@ const Home = () => (
           family. Tell them about the Folding@home project, and then join or start your own team.
           The more points your team earns, the more we can progress research on diseases like
           COVID-19, Alzheimer&apos;s disease, and cancers! If you are interested in setting up a
-          team of your own, you can get started <a href="/team">here</a>.
+          team of your own, you can get started
+          <a href="/team">here</a>
+          .
         </p>
       </div>
     </div>
