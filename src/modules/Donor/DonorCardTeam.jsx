953bf368c197142bafb3b767665775f6cbd97484
parvez <126749+parvez@users.noreply.github.com>
2021-04-27 15:17:17 -0700

    link team to team page

--- src/modules/Donor/DonorCardTeam.jsx
+++ src/modules/Donor/DonorCardTeam.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PrettyCount } from 'utils/format';
+import { Link } from 'react-router-dom';
 
 const DonorCardTeam = ({ team }) => (
   <>
-    <h4>{`${team.name} (${team.team})`}</h4>
+    <h4>
+      <Link to={`/team/${team.team}`}>
+        {`${team.name} (${team.team})`}
+      </Link>
+    </h4>
     <p>
       <span>Earned </span>
       <strong><PrettyCount count={team.score} /></strong>
