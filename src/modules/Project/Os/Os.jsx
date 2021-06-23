8ffc7ee730959b1fd40ffdf73d071faf30fc31ed
David Elizalde <david.elizalde.r.a@gmail.com>
2021-06-23 15:16:00 -0600

    Added 'Totals' row on Os table, Added a linebreak style ignore to .eslint rules. (#85)

--- src/modules/Project/Os/Os.jsx
+++ src/modules/Project/Os/Os.jsx
@@ -1,8 +1,10 @@
-import DataTable from 'elements/DataTable/DataTable';
+// import DataTable from 'elements/DataTable/DataTable';
+import { Table } from 'antd';
 import React, { useEffect } from 'react';
 import { getOs } from 'store/stats/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { PrettyCount } from 'utils/format';
+import { v4 as uuid } from 'uuid';
 
 const Os = () => {
   const dispatch = useDispatch();
@@ -71,11 +73,52 @@ const Os = () => {
   return (
     <>
       <h1>OS Statistics</h1>
-      <DataTable
+      <Table
         columns={columns}
         dataSource={stats?.os}
-        pagination={{ defaultPageSize: 10, showSizeChanger: true }}
+        pagination={false}
+        rowKey={() => uuid()}
+        size="small"
+        summary={(pageData) => {
+          let tAMD = 0;
+          let tNvidia = 0;
+          let tCPU = 0;
+          let tTFLOP = 0;
+          let tX86Flop = 0;
+          pageData.forEach((list) => {
+            tAMD += list['AMD GPUs'];
+            tNvidia += list['NVidia GPUs'];
+            tCPU += list.CPUs;
+            tTFLOP += list.TFLOPS;
+            tX86Flop += list['x86 TFLOPS'];
+          });
+          return (
+            <>
+              <Table.Summary.Row>
+                <Table.Summary.Cell>
+                  Total
+                </Table.Summary.Cell>
+                <Table.Summary.Cell align="right">
+                  <PrettyCount count={tAMD} />
+                </Table.Summary.Cell>
+                <Table.Summary.Cell align="right">
+                  <PrettyCount count={tNvidia} />
+                </Table.Summary.Cell>
+                <Table.Summary.Cell align="right">
+                  <PrettyCount count={tCPU} />
+                </Table.Summary.Cell>
+                <Table.Summary.Cell align="right">
+                  <PrettyCount count={tTFLOP} />
+                </Table.Summary.Cell>
+                <Table.Summary.Cell align="right">
+                  <PrettyCount count={tX86Flop} />
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+            </>
+          );
+        }}
       />
+      &nbsp;
       <p>
         CPUs and GPUs which have returned Work Units within the
         last 3 days are listed by OS. FLOPS per core is estimated.
